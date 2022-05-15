78c3cf259f7bd3c0e3d80a59ddf443160aa8609a
Stefan <shdutoit@yahoo.com>
2022-05-15 17:25:41 +1000

    Removed docker file Added CommandsController class Added Command class

---
+++ CommandAPI/Model/Command.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandAPI.Model
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -4,3 +4,7 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// Register support for Controllers and Routes
+builder.Services.AddControllers();
+app.MapControllers();
