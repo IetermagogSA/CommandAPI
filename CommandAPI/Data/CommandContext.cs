df36dcdc2295f5c771805ca0f16c1bc74204802a
Stefan <shdutoit@yahoo.com>
2022-05-22 16:50:12 +1000

    Added the DB Context and dependencies for Entity Framework

---
+++ CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
