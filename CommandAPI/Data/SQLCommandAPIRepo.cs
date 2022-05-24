602d765003f58b4595436b3d55afe5b0b113404d
Stefan <shdutoit@yahoo.com>
2022-05-24 20:31:58 +1000

    Added SQL implementation of CommandAPIRepo

---
+++ CommandAPI/Data/SQLCommandAPIRepo.cs
@@ -0,0 +1,42 @@
+﻿using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class SQLCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+        public SQLCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+        public void CreateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(ci => ci.Id.Equals(id));
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,12 +1,19 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Get the connection string from the appsettings into a class
+ConfigurationManager configuration = builder.Configuration;
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
 
 // Add controller support
 builder.Services.AddControllers();
 
 // Register the concrete implementation of the API Repo Interface
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+//builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SQLCommandAPIRepo>();
 
 var app = builder.Build();
 
