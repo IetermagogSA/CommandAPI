32767362a0c8e122275ad177721a12cf628eccf7
Stefan <shdutoit@yahoo.com>
2022-06-23 07:42:52 +1000

    Added unit tests for Command model as exercise Removed UnitTest1.cs created by the test project by default

---
+++ CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,65 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something awesome!",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            // Arrange
+
+            // Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            // Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            // Arrange
+
+            // Act
+            testCommand.Platform = "Changed Platform";
+
+            // Assert
+            Assert.Equal("Changed Platform",testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            // Arrange
+
+            // Act
+            testCommand.CommandLine = "Changed CommandLine";
+
+            // Assert
+            Assert.Equal("Changed CommandLine", testCommand.CommandLine);
+        }
+    }
+}
